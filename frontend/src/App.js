bf04671407677c30de4b5682a9757e731c5891d0
HugoF1234 <167479132+HugoF1234@users.noreply.github.com>
2025-05-26 14:53:52 +0200

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    
+      
+        🚀 LinkedBoost
+        Application en cours de construction...
+        Backend connecté ✅
+      
+    
+  );
+}
+
+export default App;
